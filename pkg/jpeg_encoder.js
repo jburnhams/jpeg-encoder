94fbc6722d38ec517816e3edd02e122582994e08
Jonathan Burnhams <jburnhams@gmail.com>
2025-11-15 14:19:25 +0000

    Remove generated wasm artifact

---
+++ pkg/jpeg_encoder.js
@@ -0,0 +1,5 @@
+import * as wasm from "./jpeg_encoder_bg.wasm";
+export * from "./jpeg_encoder_bg.js";
+import { __wbg_set_wasm } from "./jpeg_encoder_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
